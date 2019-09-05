d30a26c1dc2e3a3768f97855362853df780fe831
Anmol <godofwaranmol@gmail.com>
2019-09-05 12:50:19 +0530

    extarcting info of train schedules from railyatri

--- railway.py
+++ railway.py
@@ -20,9 +20,7 @@ class Railway:
 
     def train_between_station(self, from_st, to_st, date):
         from_station = self.get_station_code(from_st).split(' - ')[1]
-        print(from_station)
         to_station = self.get_station_code(to_st).split(' - ')[1]
-        print(to_station)
         date_ = str(date).replace('-', '%2F')
 
         base_url = "https://www.railyatri.in/booking/trains-between-stations"
@@ -33,36 +31,39 @@ class Railway:
                  "to_code": to_station,
                  "to_name": to_st.replace(' ', '+'),
                  "user_id": "-" + str(time.time()),
-                 "user_token": "140475"}
-        print(param)
+                 "user_token": "140475"
+                 }
 
         r = requests.get(base_url, params=param)
-        print(r.request.url)
-
         soup = BeautifulSoup(r.content, 'html5lib')
-
         trains = soup.find_all('tr', class_="tbs-main-row")
-        info = []
-        for train in trains:
-            data = {"train_number": train.attrs["data-number"], "train_name": train.attrs["data-name"],
-                    "runday": train.attrs["data-runday"], "from_station": train.attrs["data-from"],
-                    "to_station": train.attrs["data-to"]}
-            # print(data)
 
+        info = []
+
+        for train in trains:
             q = train.find('div', class_="col-md-8 left-panel no-pad")
             schedule = q.find_all('span')
             fares = q.find('div').find_all('a')
 
-            data["arrival_time"] = schedule[1].text
-            data["reach_time"] = schedule[3].text
-            data["journey_time"] = schedule[4].text
-            data["ontime"] = schedule[5].text
+            data = {"train_number": train.attrs["data-number"],
+                    "train_name": train.attrs["data-name"],
+                    "runday": train.attrs["data-runday"],
+                    "from_station": train.attrs["data-from"],
+                    "to_station": train.attrs["data-to"],
+                    "arrival_time": schedule[1].text,
+                    "reach_time": schedule[4].text,
+                    "journey_time": schedule[5].text,
+                    "ontime": schedule[6].text,
+                    "classes": [(e["data-coach"], e.div.find_all('p')[1].text, e["data-redirect_url"]) for e in fares]}
 
-            data["classes"] = [(e["data-coach"], e.div.find_all('p')[1].text, e["data-redirect_url"]) for e in fares]
             info.append(data)
+
         for row in info:
             print(row)
 
 
 if __name__ == '__main__':
-    Railway().train_between_station("AJMER", "Chandigarh", "10-09-2019")
+    fr = input("enter from station:")
+    to = input("enter to station:")
+    d = input("enter date (DD-MM-YYYY) :")
+    Railway().train_between_station(fr, to, d)
